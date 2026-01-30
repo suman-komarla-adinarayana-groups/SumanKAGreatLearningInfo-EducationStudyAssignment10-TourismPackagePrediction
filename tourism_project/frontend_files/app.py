945bb2bce54a3ea3e22a4c4dd6c10331af3a586b
suman-komarla-adinarayana-groups <suman.groups@gmail.com>
2026-01-30 15:50:44 +0000

    first commit

--- tourism_project/frontend_files/app.py
+++ tourism_project/frontend_files/app.py
@@ -5,7 +5,31 @@ import requests
 st.title("Tourism Package Prediction System")
 
 # Input fields for product and store data
-Product_Test = st.number_input("Sl No", min_value=0.0, value=12.66)
+Sl_No = st.number_input("Sl No", min_value=0.0, value=12.66)
+CustomerID	= st.number_input("Sl No", min_value=0.0, value=12.66)
+#ProdTaken	= st.number_input("Sl No", min_value=0.0, value=12.66)
+Age	= st.number_input("Sl No", min_value=0.0, value=12.66)
+TypeofContact	= st.number_input("Sl No", min_value=0.0, value=12.66)
+CityTier	= st.number_input("Sl No", min_value=0.0, value=12.66)
+DurationOfPitch = st.number_input("Sl No", min_value=0.0, value=12.66)
+Occupation	= st.number_input("Sl No", min_value=0.0, value=12.66)
+Gender	= st.number_input("Sl No", min_value=0.0, value=12.66)
+NumberOfPersonVisiting	= st.number_input("Sl No", min_value=0.0, value=12.66)
+NumberOfFollowups	= st.number_input("Sl No", min_value=0.0, value=12.66)
+ProductPitched	= st.number_input("Sl No", min_value=0.0, value=12.66)
+PreferredPropertyStar	= st.number_input("Sl No", min_value=0.0, value=12.66)
+MaritalStatus = st.number_input("Sl No", min_value=0.0, value=12.66)
+NumberOfTrips	= st.number_input("Sl No", min_value=0.0, value=12.66)
+Passport	= st.number_input("Sl No", min_value=0.0, value=12.66)
+PitchSatisfactionScore	= st.number_input("Sl No", min_value=0.0, value=12.66)
+OwnCar	= st.number_input("Sl No", min_value=0.0, value=12.66)
+NumberOfChildrenVisiting	= st.number_input("Sl No", min_value=0.0, value=12.66)
+Designation	= st.number_input("Sl No", min_value=0.0, value=12.66)
+MonthlyIncome = st.number_input("Sl No", min_value=0.0, value=12.66)
+
+
+
+Product_Test = st.number_input("Product Test", min_value=0.0, value=12.66)
 Product_Sugar_Content = st.selectbox("Product Sugar Content", ["Low Sugar", "Regular", "No Sugar"])
 Product_Allocated_Area = st.number_input("Product Allocated Area", min_value=0.0, value=0.027)
 Product_MRP = st.number_input("Product MRP", min_value=0.0, value=117.08)
@@ -17,6 +41,27 @@ Store_Age_Years = st.number_input("Store Age (Years)", min_value=0, value=16)
 Product_Type_Category = st.selectbox("Product Type Category", ["Perishables", "Non Perishables"])
 
 product_data = {
+    "Sl_No": Sl_No,
+    "CustomerID" : CustomerID,
+    "Age" : Age,
+    "TypeofContact" : TypeofContact,
+    "CityTier" : CityTier,
+    "DurationOfPitch" : DurationOfPitch,
+    "Occupation" : Occupation,
+    "Gender" : Gender,
+    "NumberOfPersonVisiting" : NumberOfPersonVisiting,
+    "NumberOfFollowups" : NumberOfFollowups,
+    "ProductPitched" : ProductPitched,
+    "PreferredPropertyStar" : PreferredPropertyStar,
+    "MaritalStatus" : MaritalStatus,
+    "NumberOfTrips" : NumberOfTrips,
+    "Passport" : Passport,
+    "PitchSatisfactionScore" : PitchSatisfactionScore,
+    "OwnCar" : OwnCar,
+    "NumberOfChildrenVisiting" : NumberOfChildrenVisiting,
+    "Designation" : Designation,
+    "MonthlyIncome" : MonthlyIncome,
+    #TO OPTIMISE
     "Product_Test": Product_Test,
     "Product_Sugar_Content": Product_Sugar_Content,
     "Product_Allocated_Area": Product_Allocated_Area,
